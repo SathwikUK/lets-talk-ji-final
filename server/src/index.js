03e474e8c1d3bb8a63e24830c2119ec3930d3649
SathwikUK <sathwiksampengala@gmail.com>
2024-10-28 21:21:43 +0530

    vercel.json for bk

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,11 @@ import authRoute from './routes/auth';
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.get("/", (req, res) => {
+  console.log("Hello, World!");
+  res.send("Hello, World!");
+});
+
 
 app.use("/auth", authRoute);
 
